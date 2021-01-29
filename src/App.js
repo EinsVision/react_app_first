aa32b5d6c77e41462fee2ffdda018a2eda4e85cf
jungchangdae <einsvision@gmail.com>
2021-01-29 21:08:24 +0900

    map function 사용 방법

--- src/App.js
+++ src/App.js
@@ -1,10 +1,30 @@
-import logo from './logo.svg';
+import React from 'react';
 import './App.css';
+
+const data = [
+  {
+    id: 1,
+    title: 'Node',
+    value: 0
+  },
+  {
+    id: 2,
+    title: 'React',
+    value: 1
+  }
+];
 
 function App() {
   return (
-    <div className="App">
+    <div className="App-header">
       Hello React!
+      {
+        data.map(ele =>(
+          <>
+            <p key={ele.id}>{ele.title}, {ele.value}</p>
+          </>
+        ))
+      }
     </div>
   );
 }
